f007298c3dcf1371cb070f7bd6086bba9516deb6
RYN BSD <rynbsd04@gmail.com>
2024-09-06 19:13:52 +0100

    add more info

--- example/src/page/home.tsx
+++ example/src/page/home.tsx
@@ -4,7 +4,13 @@ export default function home() {
   return (
     <>
       <Link to="/fetch">fetch</Link> <br />
-      <Link to="/infinite-fetch">infinite fetch</Link>
+      <Link to="/infinite-fetch">infinite fetch</Link> <br />
+      <a
+        href="https://www.npmjs.com/package/@ryn-bsd/use-fetch"
+        target="_blank"
+      >
+        npm package
+      </a>
     </>
   );
 }
